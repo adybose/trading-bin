25843232a8a83c89fc9417cd7ae8a4ed882186ef
Aditya Bose <13054902+adbose@users.noreply.github.com>
2020-12-10 03:09:11 +0530

    Add electron.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
-import { createChart } from 'lightweight-charts';
 import Graph from './Graph';
 
 function App() {
-  
-  const socket = io("http://127.0.0.1:4000");
-  socket.on('KLINE', (payload) => {
-    // console.log(payload);
-    // candleSeries.update(payload);
-  });
-
   return (
     <>
       <h1>TradingView+Binance</h1>
+      <h2>BINANCE: BTCUSDT Live Chart</h2>
       <Graph />
     </>
   );
